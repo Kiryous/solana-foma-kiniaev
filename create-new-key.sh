0c6ab457132c0f133d5b59a460d74b99e4456952
Kirill Chernakov <hi@kirys.ru>
2022-01-13 01:43:30 +0300

    refactor: handy output

--- create-new-key.sh
+++ create-new-key.sh
@@ -2,14 +2,17 @@ countfilename="./config/count.txt"
 
 if [ -f $countfilename ]; then
   saved=$(head -n 1 $countfilename)
+  # echo "Latest key count: $saved"
   current="${saved:-0}"
+  current=$((current+1))
+  # echo "Current key count: $current"
 else
-  current=0 
+  current="1"
 fi
 
 keyfilename="./keys/id-$current.json"
 
-solana-keygen new --no-bip39-passphrase --outfile $keyfilename
+solana-keygen new --no-bip39-passphrase --outfile $keyfilename >/dev/null
 private=$(head -n 1 $keyfilename)
 pubkey=$(solana-keygen pubkey $keyfilename)
 echo $current >| $countfilename
@@ -19,7 +22,13 @@ rm $keyfilename
 
 finalkeyfilename="./keys/id-$current-$pubkey.json"
 
-solana -k $finalkeyfilename airdrop 3
+solana -k $finalkeyfilename airdrop 10 >/dev/null
 
-next=$((current+1))
-echo $next >| $countfilename
+echo "🛂 Success! Here is our $current wallet with 10 SOL\n"
+echo "pubkey:"
+echo $pubkey
+echo "\nprivate:"
+cat $finalkeyfilename
+echo "\n"
+
+echo $current >| $countfilename
